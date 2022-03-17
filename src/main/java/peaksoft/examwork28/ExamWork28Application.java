6d821e64c621051f557cb576b27bda58fb903c13
krasa <jhhhk>
2022-03-17 15:13:39 +0300

    shattuk eje

---
+++ src/main/java/peaksoft/examwork28/ExamWork28Application.java
@@ -0,0 +1,13 @@
+package peaksoft.examwork28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamWork28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamWork28Application.class, args);
+    }
+
+}
---
+++ src/test/java/peaksoft/examwork28/ExamWork28ApplicationTests.java
@@ -0,0 +1,13 @@
+package peaksoft.examwork28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamWork28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
